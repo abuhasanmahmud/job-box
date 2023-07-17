3065c4906719fb99eda42ef62ab0a481ea87b47a
abuhasanmahmud <mahmud.me420@gmail.com>
2023-07-17 22:17:45 +0600

    feat(v-1) order page

--- src/pages/register/AccountCreator.js
+++ src/pages/register/AccountCreator.js
@@ -1,10 +1,10 @@
-import React from "react"
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import CandidateRegistration from './CandidateRegistration';
-import EmployerRegistration from './EmployerRegistration';
+import CandidateRegistration from "./CandidateRegistration";
+import EmployerRegistration from "./EmployerRegistration";
 
 const AccountCreator = () => {
-  console.log('all is okk')
+  // console.log('all is okk')
   const navigate = useNavigate();
   const { type } = useParams();
 
@@ -17,22 +17,22 @@ const AccountCreator = () => {
   }
 
   return (
-    <div className='h-screen pt-14'>
-      <h1 className='text-center my-10 text-2xl'>Continue as ...</h1>
-      <div className='flex justify-evenly '>
+    <div className="h-screen pt-14">
+      <h1 className="text-center my-10 text-2xl">Continue as ...</h1>
+      <div className="flex justify-evenly ">
         <div
           onClick={() => navigate("/register/candidate")}
-          className='flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-primary hover:shadow-2xl hover:scale-105 group'
+          className="flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-primary hover:shadow-2xl hover:scale-105 group"
         >
-          <img className='h-5/6' src={candidate} alt='' />
-          <p className='text-center text-3xl'>Candidate</p>
+          <img className="h-5/6" src={candidate} alt="" />
+          <p className="text-center text-3xl">Candidate</p>
         </div>
         <div
           onClick={() => navigate("/register/employer")}
-          className='flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-primary hover:shadow-2xl hover:scale-105 group'
+          className="flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-primary hover:shadow-2xl hover:scale-105 group"
         >
-          <img className='h-[77%]' src={employer} alt='' />
-          <p className='text-center text-3xl'>Employer</p>
+          <img className="h-[77%]" src={employer} alt="" />
+          <p className="text-center text-3xl">Employer</p>
         </div>
       </div>
     </div>
