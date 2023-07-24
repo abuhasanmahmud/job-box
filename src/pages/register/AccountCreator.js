e1abd84778f9a160fb7b7b0303c94b49e117bb13
abuhasanmahmud <mahmud.me420@gmail.com>
2023-07-24 22:49:15 +0600

    Feat(V-1) login page

--- src/pages/register/AccountCreator.js
+++ src/pages/register/AccountCreator.js
@@ -24,14 +24,14 @@ const AccountCreator = () => {
           onClick={() => navigate("/register/candidate")}
           className="flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-primary hover:shadow-2xl hover:scale-105 group"
         >
-          <img className="h-5/6" src={candidate} alt="" />
+          <img className="h-5/6" src="" alt="" />
           <p className="text-center text-3xl">Candidate</p>
         </div>
         <div
           onClick={() => navigate("/register/employer")}
           className="flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-primary hover:shadow-2xl hover:scale-105 group"
         >
-          <img className="h-[77%]" src={employer} alt="" />
+          <img className="h-[77%]" src="" alt="" />
           <p className="text-center text-3xl">Employer</p>
         </div>
       </div>
